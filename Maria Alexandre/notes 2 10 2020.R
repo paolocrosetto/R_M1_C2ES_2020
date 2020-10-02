d01a4ba69f97543b83c9fd0c7743c4611f4ba199
Maria Alexandre <72126192+Alec-lab@users.noreply.github.com>
2020-10-02 11:53:32 +0200

    modification

--- Maria Alexandre/notes 2 10 2020.R
+++ Maria Alexandre/notes 2 10 2020.R
@@ -138,3 +138,71 @@ select(origin, distance) %>%
 group_by(origin)%>%   
 summarise(response = mean(distance, na.rm = T))  
 variance = sd(distance, na.rm = T)
+
+
+
+
+
+
+## pour tous les vols de décembre montrer juste le delay
+df %>% 
+  filter(month == 12) %>% 
+  select(month, contains("delay"))
+
+## calucler vitesse en km/h
+df %>% 
+  select(air_time, distance) %>% 
+  mutate(distance_km = distance*1.6, time_h = air_time/60) %>% 
+  mutate(speed_km_h = distance_km/time_h)
+
+# summarise (summarize)
+# #moyenne de la vitesse des avion
+df_speed %>% 
+  summarise(mean = mean(speed_km_h, na.rm = TRUE))
+
+# meme chose du début
+df %>% 
+  select(air_time, distance) %>% 
+  mutate(distance_km = distance*1.6, time_h = air_time/60) %>% 
+  mutate(speed_km_h = distance_km/time_h) %>% 
+  summarise(mean = mean(speed_km_h, na.rm = TRUE))
+
+#vitesse maximale dans la base
+df_speed %>% 
+  summarise(max = max(speed_km_h, na.rm = TRUE))
+
+## group_by()
+## permet de faire des groupes
+## summarise va obéir à group_by()
+
+##quelle compagnie aérienne va plus vite?
+df %>% 
+  select(air_time, distance, carrier) %>% 
+  mutate(speed = distance/air_time) %>% 
+  group_by(carrier) %>% 
+  summarise(maxspeed = mean(speed, na.rm = TRUE)) %>% 
+  arrange(-maxspeed)
+
+#pourquoi? peut etre c'est lié aux distances?
+df %>% 
+  select(air_time, distance, carrier) %>% 
+  mutate(speed = distance/air_time) %>% 
+  group_by(carrier) %>% 
+  summarise(meanspeed = mean(speed, na.rm = TRUE),
+            meandist = mean(distance, na.rm = TRUE)) %>% 
+  arrange(-meanspeed)
+
+
+# what is the mean delay of flights by carrier, for each month?
+df %>% 
+  select(arr_delay, month, carrier) %>% 
+  group_by(month, carrier) %>% 
+  summarise(mean_delay = mean(arr_delay, na.rm = T))
+
+# from which airport do the longer haul flights depart?
+df %>% 
+  select(origin, distance) %>% 
+  group_by(origin) %>% 
+  summarise(reponse = mean(distance, na.rm = T),
+            vairance = sd(distance, na.rm = T))
+
