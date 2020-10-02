1a80daa60c007322a86b37690f81f59458b69809
Guillaume Faynot <guillaume.faynot@gmail.com>
2020-10-02 10:17:24 +0200

    Fonction Filter

--- GuillaumeFaynot/nycflights13.R
+++ GuillaumeFaynot/nycflights13.R
@@ -9,3 +9,11 @@ df <- flights
 read_csv("Data/HDIdata.csv")
 summary(df)
 skim(df)
+
+
+filter(df,month==12)
+
+#Tous les vols de décembre en provenance de JKF allant à LAX
+filter(df,month==12 & origin == 'JFK' & dest == 'LAX')
+#Tous les vols de décembre en provenance de JKF allant partout sauf à LAX
+filter(df,month==12 & origin == 'JFK' & dest != 'LAX')
