6319bff419e312afc5f4ff5a4ba133f2282c1f73
Jean Tavernier <jeantavernier@MacBook-Pro-de-Jean.local>
2020-10-02 10:38:06 +0200

    select

--- Jean.Tavernier/notes_L2.R
+++ Jean.Tavernier/notes_L2.R
@@ -3,4 +3,11 @@ library('tidyverse')
 df <- flights
 vol_jfk_lax <- filter(df, month == 1 & origin == 'JFK' & dest == 'LAX')
 delay <- arrange(df, month, dep_delay)
-select(df, month, day, dep_delay)
+select(df, -month, -year, dep_delay)
+select(df, starts_with("arr"))
+select(df, ends_with("y"))
+select(df, contains("y"))
+select(df, everything())
+select(df, arr_delay, dep_delay, everything())
+
+delays <- select(df, contains("delay"))
