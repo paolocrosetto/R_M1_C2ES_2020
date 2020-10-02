aa2ac0322ad5008d67b5bb5f51e57340cf3bf396
paolocrosetto <paolo.crosetto@gmail.com>
2020-10-02 10:13:04 +0200

    notes sur filter

--- Exercices par Paolo Crosetto/notes_L2.R
+++ Exercices par Paolo Crosetto/notes_L2.R
@@ -8,3 +8,26 @@ df <- flights
 
 # facon 2 d'importer: à partir d'un fichier
 hdi <- read_csv("Data/HDIdata.csv")
+
+#descriptif des données
+summary(df)
+
+# alternative pour summary des donnée
+install.packages("skimr")
+library(skimr)
+skim(df)
+
+## filter
+# cela permet de sélectionner des lignes
+
+# tous les vols de décembre
+filter(df, month == 12)
+
+# tous les vols de décembre qui partent de JFK
+filter(df, month == 12 & origin == "JFK")
+
+# tous les vols de décembre qui partent de JFK et qui vontà LAX
+vol_jfk_lax_decembre <- filter(df, month == 12 & origin == "JFK" & dest == "LAX")
+
+# tous les vols de janvier qui partent de JFK et qui ne vont pas à LAX
+filter(df, month == 1 & origin == "JFK" & dest != "LAX")
