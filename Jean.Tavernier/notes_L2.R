97eee91f0db22a794eb6ede4d850ca0b9b26eadd
Jean Tavernier <jeantavernier@MacBook-Pro-de-Jean.local>
2020-10-02 10:37:46 +0200

    select

--- Jean.Tavernier/notes_L2.R
+++ Jean.Tavernier/notes_L2.R
@@ -2,4 +2,5 @@ library('nycflights13')
 library('tidyverse')
 df <- flights
 vol_jfk_lax <- filter(df, month == 1 & origin == 'JFK' & dest == 'LAX')
-
+delay <- arrange(df, month, dep_delay)
+select(df, month, day, dep_delay)
