b21caafa4e419014513d4c563a611512eaf0e6ce
Florian sabre <Flo@wificampus-097025.grenet.fr>
2020-10-02 10:45:34 +0200

    move file

--- florian sabre/note cours.R
+++ florian sabre/note cours.R
@@ -25,8 +25,8 @@ summary(flights)
 library(skimr)
 skim(df)
 
-#Flitrer les données
-#exemple les vols de decembre:
+##Flitrer les données (voir diapo cours 21)
+#exemple les vols de decembre: selectionner les lignes 
 filter(df,month==12)
 
 #tous les vols de décembre qui partent de JFK 
@@ -34,3 +34,24 @@ filter(df,month==12 & origin=="JFK")
 #tous les vols de décembre qui partent de JFK  et qui ne vont pas à LAX 
 filter(df,month==12 & origin=="JFK" & dest!="LAX")
 
+##arrange 
+# ordonner les données par variable 
+arrange(df,month,day,dep_delay)
+
+##select
+# selectionner des colonnes 
+select (df, month,day,dep_delay)
+#selectionner toutes les colonnes sauf quelque unes
+select(def,-month,-year)
+# aide à la selection; commence par:"arr" 
+select(df, starts_with("arr"))
+#plus general, toute les variables qui contienne la lettre Y 
+select(df, contains('y'))
+#réordonner les colonnes:
+select(df,arr_delay,dep_delay,everything())
+
+#sauvegarder dans un objet les délais:
+delays <-select(df,contains("delay"))
+
+
+   
