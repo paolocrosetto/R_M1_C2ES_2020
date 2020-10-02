266de6660250ceebdb87fc4baabc321417474458
guangnan <guangnan@wificampus-071194.grenet.fr>
2020-10-02 11:42:10 +0200

    pipe

--- guangnan WEI/note10.2.R
+++ guangnan WEI/note10.2.R
@@ -68,4 +68,58 @@ select(df2,speed_miles_minute,everything())
 df3<-mutate(df2,speed_km_h=speed_miles_minute*60/1.60934)
 select(df3,starts_with("speed"))
 
+#enter the PIPE!
+## %>% # et après
+## pour tous les vols de décembre montrer juste le delay
+df %>%
+  filter(month==12) %>%
+  select(month,contains("delay"))
 
+## calculer la vitesse en km/h
+df %>%
+  select(air_time,distance) %>%
+  mutate(distance_km=distance*1.6,time_h=air_time/60) %>%
+  mutate(speed_km_h=distance_km/time_h) -> df_speed
+
+
+#summarise(summarise)
+#moyenne de la vitesse des avions
+df_speed %>%
+  select(air_time,distance) %>%
+  mutate(distance_km=distance*1.6,time_h=air_time/60) %>%
+  mutate(speed_km_h=distance_km/time_h) %>%
+  summarise(df,mean1=mean(speed_km_h,na.rm=TRUE))
+
+df_speed  %>%
+  summarise(max=max(speed_km_h,na.rm=TRUE))
+
+#group_by()
+##permet de faire des groupes
+##summarise va obéir à group_by()
+##quelle compagnie aéroenne va plus vite
+df%>%
+  select(air_time,distance,carrier)%>%
+  mutate(speed=distance/air_time)%>%
+  group_by(carrier)%>%
+  summarise(maxspeed=mean(speed,na.rm=TRUE))%>%
+  arrange(-maxspeed)
+
+#pourquoi? peut être c'est lié aux distances?
+df%>%
+  select(air_time,distance,carrier)%>%
+  mutate(speed=distance/air_time)%>%
+  group_by(carrier)%>%
+  summarise(maxspeed=mean(speed,na.rm=TRUE),meandist=mean(distance,na.rm=TRUE))%>%
+  arrange(-maxspeed)
+
+#what is the mean delay of flights by carrier,for each month?
+df %>%
+  select(arr_delay,month,carrier)%>%
+  group_by(month,carrier)%>%
+  summarise(mean_delay=mean(arr_delay,na.rm = T))
+
+#from which airport do the longer haul flights depart?
+df %>%
+  select(origin,distance)%>%
+  group_by(origin)%>%
+  summarise(reponse=mean(distance,na.rm = T),variance=sd(distance,na.rm = T))
