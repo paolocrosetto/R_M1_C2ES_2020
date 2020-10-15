532e8b36ac33c9c72e5290ddec1518a3c0ff0139
Maria Alexandre <72126192+Alec-lab@users.noreply.github.com>
2020-10-15 12:53:13 +0200

    chgt

--- Maria Alexandre/notes 15 10 2020.R
+++ Maria Alexandre/notes 15 10 2020.R
@@ -70,7 +70,7 @@ flights %>%
   geom_histogram() 
  
 
-Histogramm avec un nombre de bins
+#Histogramm avec un nombre de bins
 
 flights %>%
   ggplot(aes(x=dep_time)) + 
@@ -107,3 +107,126 @@ flights %>%
   ggplot(aes(carrier)) + 
   geom_bar(aes(color=origin, fill=origin),
            position= position_dodge())
+
+#Deux variables
+
+#variable cont,et variable  cont
+#scatterplot -> nuage de point
+mpg %>%
+  ggplot(aes(cty, hwy))+
+  geom_point()                           #plusieurs point aux même endroits passe des points sous silence
+#scatterplot avec jitter
+mpg %>%
+  ggplot(aes(cty, hwy))+                 # inclus du bruits =< ecartes les points qui sont les uns sur les autres 
+  geom_jitter() 
+
+mpg %>%
+  ggplot(aes(cty, hwy))+                
+  geom_jitter(width=8, height=8) 
+
+#Smooth -> ajouter une tendance
+mpg %>%
+  ggplot(aes(cty, hwy))+                
+  geom_smooth()
+mpg %>%
+  ggplot(aes(cty, hwy))+                
+  geom_smooth() +
+  geom_point()
+
+#ou
+mpg %>%
+  ggplot(aes(cty, hwy))+                
+  geom_smooth() +
+  geom_jitter()
+
+#Variable cont , et variable discrète
+
+## Consommation en ville par constructeur
+
+#Boxplot
+mpg %>%
+  ggplot(aes(x=manufacturer, y= cty))+                
+  geom_boxplot() 
+##Violin
+mpg %>%
+  ggplot(aes(x=drv, y= cty))+                
+  geom_violin() 
+
+mpg %>%
+  ggplot(aes(x=drv, y= cty))+                
+  geom_violin(aes(fill=drv)) 
+
+#barres -> colonnes
+#nb de vols par compagnies aériennes
+
+#Version 1 : seule 1 variable -> geam_bar
+flights %>%
+  ggplot(aes(carrier))+                
+  geom_bar() 
+
+#Version 2 : avec 2 variables -> geam_col
+flights %>%
+  group_by(carrier) %>%                
+  summarise(n = n()) %>%
+  ggplot(aes(x=carrier, y= n)) +
+  geom_col() 
+
+
+flights %>%
+  group_by(carrier) %>%                
+  summarise(n = n()) %>%
+  ggplot(aes(x=reorder(carrier,-n), y= n)) +
+  geom_col() 
+
+#Quelles compagnie a plus de retard en moyenne ?
+flights %>%
+  group_by(carrier) %>%                
+  summarise(mean_delay= mean(arr_delay, na.rm = T)) %>%
+  ggplot(aes(x=carrier, y= mean_delay)) +
+  geom_col()
+
+flights %>%
+  group_by(carrier) %>%                
+  summarise(mean_delay= mean(arr_delay, na.rm = T)) %>%
+  ggplot(aes(x=reorder(carrier, -mean_delay), y= mean_delay)) +
+  geom_col()
+
+flights %>%
+  group_by(carrier) %>%                
+  summarise(mean_delay= mean(arr_delay, na.rm = T)) %>%
+  ggplot(aes( y = reorder(mean_delay,mean_delay), x=mean_delay)) +
+  geom_col()
+
+flights %>%
+  group_by(carrier) %>%                
+  summarise(mean_delay= mean(arr_delay, na.rm = T)) %>%
+  ggplot(aes( y = reorder(mean_delay,mean_delay), x=mean_delay)) +
+  geom_point()
+
+##
+#2 variables discrètes, discrète
+#Est ce que certains producteurs se concentrent sur un segment
+mpg %>%
+  ggplot(aes(manufacturer, class))+                
+  geom_count() 
+#Quelles compagnies partent de quels aéroports
+flights %>%
+  ggplot(aes( y = carrier, x=origin)) +
+  geom_count()
+
+#3 variables
+
+# geom_tile
+#Nombre de vols par origine et par destination
+flights %>%
+  group_by(origin,dest)%>%
+  summarise(n = n()) %>%
+  ggplot(aes( x=dest, y = origin, fill = n)) +
+  geom_tile()
+#pour plus de 1000 vols
+flights %>%
+  group_by(origin,dest)%>%
+  summarise(n = n()) %>%
+  filter()
+ggplot(aes( x=dest, y = origin, fill = n)) +  
+  geom_tile()
